007549845ba6f6abc390386ca926456d5bf2de05
Sonu Singh <bhpcv252@gmail.com>
2025-05-31 12:09:14 +0530

    added leaflet images

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: "/leaflet/marker-icon.png",
+    iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+    shadowUrl: "/leaflet/marker-shadow.png",
+    layerUrl: "/leaflet/layer.png",
+    layerRetinaUrl: "/leaflet/layer-2x.png",
+});
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <App />
